7d768445f3d27976775b9e53de8b5a75a44f24d2
11philip22 <philipwoldhek@gmail.com>
2019-08-21 10:26:46 +0200

    2019-08-21_10:26:46

--- deploy-scripts.sh
+++ deploy-scripts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
-username=philip
-ln -s /home/${username}/scripts/sshuttle.py /usr/local/bin/vpn
-ln -s /home/${username}/scripts/autogit.sh /usr/local/bin/autogit
+script_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ln -s ${script_root}/sshuttle.py /usr/local/bin/vpn
+ln -s ${script_root}/autogit.sh /usr/local/bin/autogit
