92eac4d15827e87facea9a922bf9d387149c49bc
11philip22 <philipwoldhek@gmail.com>
2019-08-21 10:31:42 +0200

    2019-08-21_10:31:42

--- deploy-scripts.sh
+++ deploy-scripts.sh
@@ -4,3 +4,12 @@
 script_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ln -s ${script_root}/sshuttle.py /usr/local/bin/vpn
 ln -s ${script_root}/autogit.sh /usr/local/bin/autogit
+
+if [[ $1 = "clean" ]]; then
+    if [ "$EUID" -ne 0 ]; then
+        echo "Please run as root"
+        exit
+    fi
+    rm /usr/local/bin/vpn
+    rm /usr/local/bin/autogit
+fi
