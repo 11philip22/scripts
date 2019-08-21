168519dbaac126470de54153428cd5b1b127d61d
11philip22 <philipwoldhek@gmail.com>
2019-08-21 10:40:49 +0200

    2019-08-21_10:40:49

--- deploy-scripts.sh
+++ deploy-scripts.sh
@@ -6,11 +6,16 @@ if [[ "$EUID" -ne 0 ]]; then
     exit
 fi
 
+
 if [[ $1 = "clean" ]]; then
     rm /usr/local/bin/vpn
     rm /usr/local/bin/autogit
-else
+
+elif [[ $1 = "install" ]]; then
     script_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
     ln -s ${script_root}/sshuttle.py /usr/local/bin/vpn
     ln -s ${script_root}/autogit.sh /usr/local/bin/autogit
+
+else
+    echo "Usage: sudo ./deploy-scripts.sh install/clean"
 fi
