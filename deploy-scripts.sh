20cbe26f151b14352bf413de7274e24e0acd984a
11philip22 <philipwoldhek@gmail.com>
2019-08-27 13:09:45 +0200

    2019-08-27_13:09:45

--- deploy-scripts.sh
+++ deploy-scripts.sh
@@ -15,7 +15,8 @@ elif [[ $1 = "install" ]]; then
     script_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
     ln -s ${script_root}/sshuttle.py /usr/local/bin/vpn
     ln -s ${script_root}/autogit.sh /usr/local/bin/autogit
-
+    ln -s ${script_root}/kpndisplay.sh /usr/local/bin/kpndisplay
+    
 else
     echo "Usage: sudo ./deploy-scripts.sh install/clean"
 fi
